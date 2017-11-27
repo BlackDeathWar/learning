4bc94581962859b24a8e988005c3fd8d03fc342a
Youssef Abdelfatah <youssefabdelfatah77@gmail.com>
2017-11-27 23:00:41 +0200

    init

---
+++ main.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// logical operators
+// ==   equal
+// !=   not equal
+// ||   or
+// &&   and
+// >
+// <
+// >=
+// <=
+
+// 0 = false
+// ay rkam 8ar zero = true
+
+// and
+// 1 0 = 0
+// 0 1 = 0
+// 0 0 = 0
+// 1 1 = 1
+
+int main()
+{
+    /*
+
+    if(condition)
+     {
+        code;
+     }
+     else
+     {
+        code;
+     }
+    */
+
+    // b3raf password hna
+    int password = 9999;
+
+    int user;
+    scanf ("%d",&user);
+
+    if(password == user)
+    {
+        printf("the password is correct\n");
+    }
+    else
+    {
+        printf("the password is incorrect\n");
+    }
+
+
+
+
+    return 0;
+
+}
